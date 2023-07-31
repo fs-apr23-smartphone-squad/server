3d12f76cd0a582565b96cbc7266d91cfd42fbc3a
Igor Shevtsov <contact.shevtsov@gmail.com>
2023-07-31 12:36:53 +0300

    corrected console.log

--- src/server.ts
+++ src/server.ts
@@ -27,5 +27,5 @@ app.get('/products', (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log('Server is running on api.smartphonesquad.shop 🚀🚀🚀');
+  console.log('Server is running on http://localhost:5000 🚀🚀🚀');
 });
