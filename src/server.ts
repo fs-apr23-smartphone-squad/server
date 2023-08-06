e91a65f0f6852fe9ae8cb6e2e7144990c47b488a
Igor Shevtsov <contact.shevtsov@gmail.com>
2023-08-06 21:54:04 +0300

    fix deploy

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import cors from 'cors';
 import { initDB } from './initDB';
 import { productRoutes } from './routes/product.routes';
 
-const PORT = 5001;
+const PORT = 5000;
 
 const app = express();
 app.use(cors());
