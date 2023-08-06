6d41f31698d430a91965bce318554dccc00d930f
Shevtsooov <97849888+Shevtsooov@users.noreply.github.com>
2023-08-06 23:06:38 +0300

    Update and rename 20230731172946-Products.js to 20230806122349-create-products.js

--- migrations/20230806122349-create-products.js
+++ migrations/20230806122349-create-products.js
@@ -1,60 +1,68 @@
 'use strict';
 
-const TABLE_NAME = 'Products';
+const TABLE_NAME = 'products';
 
 module.exports = {
-  async up(queryInterface, Sequelize) {
+  async up (queryInterface, Sequelize) {
     await queryInterface.createTable(TABLE_NAME, {
       id: {
-        type: Sequelize.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
         allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
       },
+
       category: {
         type: Sequelize.STRING,
         allowNull: false,
       },
-      phoneId: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
+      
       itemId: {
         type: Sequelize.STRING,
         allowNull: false,
+        unique: true,
       },
+
       name: {
         type: Sequelize.STRING,
         allowNull: false,
       },
+
       fullPrice: {
         type: Sequelize.INTEGER,
         allowNull: false,
       },
+
       price: {
         type: Sequelize.INTEGER,
         allowNull: false,
       },
+
       screen: {
         type: Sequelize.STRING,
         allowNull: false,
       },
+
       capacity: {
         type: Sequelize.STRING,
         allowNull: false,
       },
+
       color: {
         type: Sequelize.STRING,
         allowNull: false,
       },
+
       ram: {
         type: Sequelize.STRING,
         allowNull: false,
       },
+
       year: {
         type: Sequelize.INTEGER,
         allowNull: false,
       },
+
       image: {
         type: Sequelize.STRING,
         allowNull: false,
@@ -62,7 +70,7 @@ module.exports = {
     });
   },
 
-  async down(queryInterface, Sequelize) {
+  async down (queryInterface, Sequelize) {
     await queryInterface.dropTable(TABLE_NAME);
-  },
+  }
 };
